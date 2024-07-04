395263a8515be25a3d9db670133a64a013ec7a46
Kokila-2001 <kokilajayasinha66@gmail.com>
2024-07-04 16:41:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed(".multiple-text",{
+    Strings: ["Frontend Developer", "You Tuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
